2a2fe294af002b0f15f7b0faa58faa9541422037
Maciej Nowak <maciejt.nowak@gmail.com>
2022-02-21 20:06:54 +0100

    pip-installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qemu-runner',
+    version='1.0.0',
+    packages=find_packages(where='src'),
+    package_dir={
+        '': 'src'
+    },
+    package_data={
+        'qemu_runner.make_runner': ['main.py.in']
+    },
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'qemu_make_runner=qemu_runner.make_runner.__main__:run'
+        ]
+    }
+)
--- src/qemu_runner/make_runner/__main__.py
+++ src/qemu_runner/make_runner/__main__.py
@@ -1,19 +1,26 @@
 import argparse
+import sys
+from typing import List
 
 from .make import make_runner, load_layers_from_all_search_paths
 
 
-def parse_args():
+def parse_args(argv: List[str]):
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--layers', nargs='+', required=True, help='Layer files')
     parser.add_argument('-o', '--output', required=True, help='Output .pyz file', type=argparse.FileType('wb'))
-    return parser.parse_args()
+    return parser.parse_args(argv)
 
 
-def main(args: argparse.Namespace):
+def main(argv: List[str]):
+    args = parse_args(argv)
     layer_contents = load_layers_from_all_search_paths(args.layers)
     make_runner(args.output, layer_contents)
 
 
+def run():
+    main(sys.argv[1:])
+
+
 if __name__ == '__main__':
-    main(parse_args())
+    run()

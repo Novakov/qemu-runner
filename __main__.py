37792565ae9da2131e56be5f8b78f77fa19bed14
Maciej Nowak <maciejt.nowak@gmail.com>
2022-04-04 19:34:37 +0200

    Automated versioning and package publish

--- __main__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-import qemu_runner.run
-qemu_runner.run.main()
--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
+import os.path
+
 from setuptools import setup, find_packages
+
+
+with open(os.path.dirname(os.path.abspath(__file__)) + '/README.md', 'r') as f:
+    description = f.read()
 
 setup(
     name='qemu-runner',
     version='1.0.0',
+    description='Create self-contained wrappers around QEMU to hide & share long command-line invocations',
+    url='https://github.com/Novakov/qemu-runne',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Maciej Nowak',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: System :: Emulators',
+        'Topic :: Utilities',
+    ],
     packages=find_packages(where='src'),
     package_dir={
         '': 'src'
     },
-    # package_data={
-    #     'qemu_runner.make_runner': ['main.py.in']
-    # },
     include_package_data=True,
     python_requires=">=3.8",
     entry_points={
